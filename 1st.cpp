ce28a4034e0bf63fae6eef00703da1b70ef17505
kushal-022 <105519854+kushal-022@users.noreply.github.com>
2022-05-16 23:58:20 +0530

    Add files via upload

---
+++ 1st.cpp
@@ -0,0 +1,40 @@
+#include "bits/stdc++.h"
+using namespace std;
+int main(){
+
+    int n , m ;
+    cin>>n>>m;
+    int arr[n][m];
+    for(int i= 0 ; i<n ; i++){
+        for(int j = 0 ; j<m ; j++){
+            cin>>arr[i][j];
+        }
+    }
+
+    int target ; 
+    cin>>target;
+    bool found = false ;
+
+    int r = 0 , c = m - 1 ;
+    while(r<n and c>=0){
+
+        if(arr[r][c]==target){
+            found = true ;
+        }
+        if(arr[r][c]>target){
+            c--;
+        }
+        else{
+            r++;
+        }
+    }
+
+    if(found){
+        cout<<"Element found "<<endl;
+    }
+    else{
+        cout<<"Element not found "<<endl;
+    }
+
+    return 0 ;
+}
